4436b9cb941c4f52bcded77a891aae035cae2dfc
Tomer Harari <tomerdb@gmail.com>
2020-11-03 16:46:31 +0200

    added build folder to git

---
+++ build/precache-manifest.4737778f8c07f0353ea6d6d5033bf804.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c61d11c615bf83b4eed52f31efb0b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d01ff8457c943742b9ac",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "270fb60487c45e45d9c6",
+    "url": "/static/css/main.f504af29.chunk.css"
+  },
+  {
+    "revision": "d01ff8457c943742b9ac",
+    "url": "/static/js/2.9bd3fb07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9bd3fb07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "270fb60487c45e45d9c6",
+    "url": "/static/js/main.f930c8ac.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "8f8fec6bc03fa13290fd69311a02320f",
+    "url": "/static/media/amazon-clone.8f8fec6b.png"
+  },
+  {
+    "revision": "34847a22fc640e706c859457f0475487",
+    "url": "/static/media/google-store-loactor-clone.34847a22.png"
+  },
+  {
+    "revision": "fe0dcf2a4abc1be3c5cfd6bb2ce6ec3d",
+    "url": "/static/media/header-background.fe0dcf2a.jpg"
+  },
+  {
+    "revision": "1da8d950e3f30a69d7c957c26238e1de",
+    "url": "/static/media/icons8-firebase.1da8d950.svg"
+  },
+  {
+    "revision": "409452f1b3c56ada4efdcfbd39ff1142",
+    "url": "/static/media/icons8-mongodb.409452f1.svg"
+  },
+  {
+    "revision": "d4b168583c924b07fa6fdaf682886bac",
+    "url": "/static/media/profile3.d4b16858.jpeg"
+  },
+  {
+    "revision": "1c35661cc90b1f2f027ae80db52f2558",
+    "url": "/static/media/twitter-clone.1c35661c.png"
+  }
+]);
